490016dcc6732d83b69a18796d3631eeda5c978d
boyyang-love <boyyang-love@outlook.com>
2020-05-06 21:33:13 +0800

    first commit

---
+++ precache-manifest.25638a4c9456f5b9ecdbb01c64b9a1da.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1176b3c86e7c179b4663fb50a5f7d83",
+    "url": "index.html"
+  },
+  {
+    "revision": "27b1ab3fec5c4c6e5b08437fcc4becdc",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "036a482aff4a870e895e",
+    "url": "static/css/app.ca936927.css"
+  },
+  {
+    "revision": "8d9840d9cc8eb6413c7b",
+    "url": "static/css/chunk-778be118.9a30ba71.css"
+  },
+  {
+    "revision": "bc32e9e45fb2bbcd075c",
+    "url": "static/css/chunk-77a80d86.5040c9b2.css"
+  },
+  {
+    "revision": "036a482aff4a870e895e",
+    "url": "static/js/app.bf3eea56.js"
+  },
+  {
+    "revision": "943cdd5963adfb0520a2",
+    "url": "static/js/chunk-3a8d837b.805b2d5b.js"
+  },
+  {
+    "revision": "8d9840d9cc8eb6413c7b",
+    "url": "static/js/chunk-778be118.74fb0ef5.js"
+  },
+  {
+    "revision": "bc32e9e45fb2bbcd075c",
+    "url": "static/js/chunk-77a80d86.9cb85a8e.js"
+  },
+  {
+    "revision": "184c7e8f5dc01ba51ba3",
+    "url": "static/js/chunk-vendors.85e6919b.js"
+  }
+]);
